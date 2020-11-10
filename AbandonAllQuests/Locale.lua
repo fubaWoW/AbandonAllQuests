0aacb927a4f524014c3b9ca62406902f79a0a747
Ole Scheller <olescheller87@gmail.com>
2020-11-10 18:09:09 +0100

    Add bundle script

---
+++ AbandonAllQuests/Locale.lua
@@ -0,0 +1,22 @@
+local ADDON_NAME, namespace = ...
+
+local locale = GetLocale();
+
+local translations = {}
+translations.enUS = {
+    MAP_BUTTON_LABEL = "Abandon all quests",
+    ABANDON_DIALOG_QUESTION = "Are you sure you want to abandon all quests?",
+    ABANDON_QUEST_SUCCESS = "Abandoned quest: %s",
+}
+
+translations.deDE = {
+    MAP_BUTTON_LABEL = "Alle quests abbrechen",
+    ABANDON_DIALOG_QUESTION = "Bist du sicher, dass du alle Quests abbrechen willst?",
+    ABANDON_QUEST_SUCCESS = "Quest abgebrochen: %s",
+}
+
+function AbandonAllQuests_GetTranslation(key) 
+    return translations[locale][key] or translations["enUS"][key]
+end
+
+namespace.AbandonAllQuests_GetTranslation = AbandonAllQuests_GetTranslation;
---
+++ bundle.sh
@@ -0,0 +1,5 @@
+mkdir AbandonAllQuests
+cp AbandonAllQuests.lua AbandonAllQuests/
+cp AbandonAllQuests.toc AbandonAllQuests/
+cp Locale.lua AbandonAllQuests/
+tar -a -c -f AbandonAllQuests.zip AbandonAllQuests/
