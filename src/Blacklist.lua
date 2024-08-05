e804dc035e5ef56852ac382909a14717d1d06e83
fuba <q3fuba@gmail.com>
2024-08-05 15:00:27 +0200

    Added many missing "Skipping Quests" for Raids!

--- src/Blacklist.lua
+++ src/Blacklist.lua
@@ -2,24 +2,38 @@ local ADDON_NAME, namespace = ...
 
 namespace.BlackListTable = {
 	24756, -- Blood Infusion (DK Legendary Quest from WotLK)
-	
+
 	29433, -- Test Your Strength (Darkmoon Quest)
+
+	78421, -- The Power of Dreams, Amirdrasil Quest for Head Enchant
+
+	----------------------------------------------
+	-- all possible "Skipping Quests" for Raids --
+	----------------------------------------------
 
 	37029, -- Blackrock Foundry (Normal)
 	37030, -- Blackrock Foundry (Heroic)
 	37031, -- Blackrock Foundry (Mythic)
 
-	39499, -- Hellfire Citadel (Normal)
-	39500, -- Hellfire Citadel (Heroic)
-	39501, -- Hellfire Citadel (Mythic)
+	39499, -- Hellfire Citadel: Well of Souls (Normal)
+	39500, -- Hellfire Citadel: Well of Souls (Heroic)
+	39501, -- Hellfire Citadel: Well of Souls (Mythic)
 
-	45381, -- Nighthold (Normal)
-	45382, -- Nighthold (Heroic)
-	45383, -- Nighthold (Mythic)
+	39502, -- Hellfire Citadel: The Fel Spire (Normal)
+	39504, -- Hellfire Citadel: The Fel Spire (Heroic)
+	39505, -- Hellfire Citadel: The Fel Spire (Mythic)
 
-	47725, -- Tomb of Sargeras (Normal)
-	47726, -- Tomb of Sargeras (Heroic)
-	47727, -- Tomb of Sargeras (Mythic)
+	44283, -- The Emerald Nightmare: Piercing the Veil (Normal)
+	44284, -- The Emerald Nightmare: Piercing the Veil (Heroic)
+	44285, -- The Emerald Nightmare: Piercing the Veil (Mythic)
+
+	45381, -- The Nighthold: Talisman of the Shal'dorei (Normal)
+	45382, -- The Nighthold: Talisman of the Shal'dorei (Heroic)
+	45383, -- The Nighthold: Talisman of the Shal'dorei (Mythic)
+
+	47725, -- Tomb of Sargeras: Aegwynn's Path (Normal)
+	47726, -- Tomb of Sargeras: Aegwynn's Path (Heroic)
+	47727, -- Tomb of Sargeras: Aegwynn's Path (Mythic)
 
 	49032, -- Antorus, the Burning Throne: Dark Passage (Normal)
 	49075, -- Antorus, the Burning Throne: Dark Passage (Heroic)
@@ -29,9 +43,27 @@ namespace.BlackListTable = {
 	49134, -- Antorus, the Burning Throne: The Heart of Argus (Heroic)
 	49135, -- Antorus, the Burning Throne: The Heart of Argus (Mythic)
 
+	58373, -- Ny'alotha: MOTHER's Guidance (Normal)
+	58374, -- Ny'alotha: MOTHER's Guidance (Heroic)
+	58375, -- Ny'alotha: MOTHER's Guidance (Mythic)
+
 	64597, -- Sanctum of Domination (Normal)
 	64598, -- Sanctum of Domination (Heroic)
 	64599, -- Sanctum of Domination (Mythic)
 
-	78421  -- The Power of Dreams, Amirdrasil Quest for Head Enchant
+	65762, -- Sepulcher of the First Ones - Heavy is the Crown (Mythic)
+	65763, -- Sepulcher of the First Ones - Heavy is the Crown (Heroic)
+	65764, -- Sepulcher of the First Ones - Heavy is the Crown (Normal)
+
+	71018, -- Vault of the Incarnates: Break a Few Eggs (Normal)
+	71019, -- Vault of the Incarnates: Break a Few Eggs (Heroic)
+	71020, -- Vault of the Incarnates: Break a Few Eggs (Mythic)
+
+	76083, -- Aberrus, the Shadowed Crucible: Echoes of the Earth-Warder (Normal)
+	76085, -- Aberrus, the Shadowed Crucible: Echoes of the Earth-Warder (Heroic)
+	76086, -- Aberrus, the Shadowed Crucible: Echoes of the Earth-Warder (Mythic)
+
+	78600, -- Amirdrassil, the Dream's Hope: Up in Smoke (Normal)
+	78601, -- Amirdrassil, the Dream's Hope: Up in Smoke (Heroic)
+	78602  -- Amirdrassil, the Dream's Hope: Up in Smoke (Mythic)
 }
